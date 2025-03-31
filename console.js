a0937a7b078553dfbd94b9bf79cc2dad85e0e487
Nahom Haileselassie <66105821+CRXNCM@users.noreply.github.com>
2025-03-31 17:13:49 +0300

    Create console.js

---
+++ console.js
@@ -0,0 +1,38 @@
+// console.js
+
+// List of emojis
+const emojis = ['😀', '😂', '🥳', '😎', '🤔', '😴', '🤖', '👻', '🐱', '🌟'];
+
+// Function to display emojis
+function displayEmojis(emojiList) {
+    console.log('Current Emojis:', emojiList.join(' '));
+}
+
+// Function to shuffle emojis randomly
+function shuffleEmojis(emojiList) {
+    for (let i = emojiList.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [emojiList[i], emojiList[j]] = [emojiList[j], emojiList[i]];
+    }
+    return emojiList;
+}
+
+// Function to add a random emoji
+function addRandomEmoji(emojiList) {
+    const randomEmoji = String.fromCodePoint(0x1F600 + Math.floor(Math.random() * 80));
+    emojiList.push(randomEmoji);
+    return emojiList;
+}
+
+// Main program
+console.log('Welcome to the Random Emoji Console!');
+let currentEmojis = [...emojis];
+displayEmojis(currentEmojis);
+
+// Perform random actions
+const actions = [shuffleEmojis, addRandomEmoji];
+setInterval(() => {
+    const randomAction = actions[Math.floor(Math.random() * actions.length)];
+    currentEmojis = randomAction(currentEmojis);
+    displayEmojis(currentEmojis);
+}, 2000);
